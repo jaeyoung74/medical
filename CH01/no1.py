b5d1bb4685e72f51632d5b8d1c7532a9b18d0222
goorm <goorm@goormui-MacBookPro.local>
2026-01-12 23:16:40 +0900

    의료데이터분석스터디

---
+++ CH01/no1.py
@@ -0,0 +1,27 @@
+#데이터 담기
+#1
+name = "환자"
+age = 20
+tmp = 36.5
+bp = 100
+diabetes = False
+
+print(name,age,tmp,bp,diabetes)
+
+#2
+list = [100, 90, 80]
+print(list)
+print(list[0])
+print(list[1])
+print(list[2])
+
+#3
+dict = {
+    'name': "환자",
+    'age': 20,
+    'tmp': 36.5,
+    'bp': 100,
+    'diabetes': "False"
+}
+
+print(dict)
